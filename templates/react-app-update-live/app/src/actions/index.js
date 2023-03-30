1e55294196048c6bc30e782a0415d3f67f255f34
Dylan :D <118640913+dylan-yakson@users.noreply.github.com>
2023-03-30 16:49:43 -0500

    Delete templates/react-app-update-live/app directory

--- templates/react-app-update-live/app/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export const submissionType = (subType) => ({
-  type: "SUBMISSION_TYPE",
-  payload: subType,
-});
-
-export const getMinimums = (minimums) => ({
-  type: "GET_MINIMUMS",
-  payload: minimums,
-});
